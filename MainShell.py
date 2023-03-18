1fba7536c0bf4756093b8d6e111177e82288db74
Mustafa Melih Can <mustafamelihcan@gmail.com>
2023-03-18 14:46:46 +0300

    added the code to open the browser and the link

--- MainShell.py
+++ MainShell.py
@@ -1,12 +1,27 @@
-import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.kuranmeali.com/AyetKarsilastirma.php?sure=2&ayet=3")
+
+time.sleep(10)
 
 
-def is_venv():
-    return (hasattr(sys, 'real_prefix') or
-            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
 
 
-if is_venv:
-    print('inside a venv')
-else: 
-    print('not inside a virtual env')
+""""
+PATH = '/Users/mustafamelihcan/Applications/chromedriver'
+
+driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(ChromeDriverManager(PATH).install())
+
+driver.get('https://www.kuranmeali.com/AyetKarsilastirma.php?sure=2&ayet=3') 
+
+
+
+
+
+"""
