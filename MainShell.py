39d4ff0f6a17cd6ed40390861b852d5100db48fc
Mustafa Melih Can <mustafamelihcan@gmail.com>
2023-04-13 14:56:00 +0300

    figured out how to take an object

--- MainShell.py
+++ MainShell.py
@@ -1,15 +1,19 @@
+
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.kuranmeali.com/AyetKarsilastirma.php?sure=2&ayet=3")
 
-time.sleep(10)
-
-
+#time.sleep(5)
+#print(driver.title)
+Ahmet_Varol = driver.find_element(By.ID,'ahmetvarol')
+print(Ahmet_Varol)
+driver.close
 
 
 """"
@@ -19,9 +23,5 @@ driver = webdriver.Chrome(PATH)
 driver = webdriver.Chrome(ChromeDriverManager(PATH).install())
 
 driver.get('https://www.kuranmeali.com/AyetKarsilastirma.php?sure=2&ayet=3') 
-
-
-
-
 
 """
